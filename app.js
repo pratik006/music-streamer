01cde3c74b833fe19bea22d26fc3bcf38a5159b3
Pratik Sengupta <pratik006@gmail.com>
2018-06-24 01:25:30 +0530

    fixed path pattern issue

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const CONTEXT = "http://192.168.0.222:8090/";
+//const CONTEXT = "http://192.168.0.222:8090/";
+const CONTEXT = "http://192.168.0.222/";
 const URL = "./lib.json";
 const SEARCH_ACTIVATE_LEN = 0;
 const searchField = document.querySelector("#search");
@@ -12,7 +13,24 @@ searchField.addEventListener("input", (evt) => {
     }
 });
 
-function play1(songDiv, song) {
+(function() {
+    fetch(URL, {
+        headers: {
+            "Authorization": "Basic ftpuser:peed"
+        }
+    }).then(resp=>resp.json())
+        .then(resp => resp.map(resp => resp.replace("E:/Songs/", "")))
+        .then(resp => {songList = resp; return resp;})
+        //.then(resp => resp.filter(resp => resp.includes("\\Arijit\\")))
+        .then(resp => resp.slice(0, 10))
+        
+        .then(songs => {
+            //console.log(songs);
+            viewpane.innerHTML = songs.map(song => createCard(song)).join("");
+        });
+})();
+
+function play(songDiv, song) {
     songDiv.innerHTML = `<audio controls autoplay="">
         <source src="${CONTEXT + encodeURI(song)}" type="audio/mpeg" />
     </audio>`;
@@ -24,51 +42,20 @@ async function searchSongs(searchKey) {
     viewpane.innerHTML = songList.filter(song => song.toUpperCase().includes(searchKey.toUpperCase())).slice(0, 10).map(song => createCard(song)).join("");
 }
 
-(function() {
-    console.log("init");
-
-    fetch(URL, {
-        headers: {
-            "Authorization": "Basic ftpuser:peed"
-        }
-    }).then(resp=>resp.json())
-        .then(resp => resp.map(resp => resp.replace("E:\\Songs\\", "")))
-        .then(resp => {songList = resp; return resp;})
-        //.then(resp => resp.filter(resp => resp.includes("\\Arijit\\")))
-        .then(resp => resp.slice(0, 10))
-        
-        .then(songs => {
-            //console.log(songs);
-            songs.forEach(song => document.querySelector(".container > .albums").innerHTML = document.querySelector(".container > .albums").innerHTML+createCard(song));
-        });
-})();
-
-
 function createCard(song) {
+    const name = song.replace("/^.*[\\\/]/", '');
     return `
         <div class="col-lg-4 col-sm-6 portfolio-item">
             <div class="card h-100">
             <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
             <div class="card-body">
                 <h4 class="card-title">
-                <a href="#">${song.replace("/^.*[\\\/]/", '')}</a>
+                <a href="#">${name}</a>
                 </h4>
                 <p class="card-text"></p>
-                <div class="player-${song}" ><button onclick="play1(this.parentElement, '${song}')";><i class="fa fa-play"></i></button></div>
-                <!--audio controls>
-                    <source src="${CONTEXT + encodeURI(song)}" type="audio/mpeg" />
-                </audio-->
-                <!--video controls="" autoplay="" name="media">
-                    <source src="${encodeURI(song)}" type="audio/mpeg">
-                </video-->
+                <div><button onclick="play(this.parentElement, \'${song}\')";><i class="fa fa-play"></i></button></div>
             </div>
             </div>
         </div>
     `;
-}
-
-function play(song) {
-    return `<audio controls autoplay="">
-        <source src="${CONTEXT + encodeURI(song)}" type="audio/mpeg" />
-    </audio>`;
 }
