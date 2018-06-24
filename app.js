65d1853642274719f57156b699de0dab3b4c9ec3
pratik <Pratik.Sengupta@in.pega.com>
2018-06-24 21:26:58 +0530

    UI changes

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
 //const CONTEXT = "http://192.168.0.222:8090/";
 const CONTEXT = "http://192.168.0.222/";
 const URL = "./lib.json";
+const PAGE_SIZE = 12;
 const SEARCH_ACTIVATE_LEN = 0;
+const RECENT_LIST_KEY = "recentList";
+const RECENT_QUEUE_LEN = 10;
+
 const searchField = document.querySelector("#search");
-const viewpane = document.querySelector(".container > .albums")
+const formField = document.querySelector("#my-form");
+const searchResultField = document.querySelector("#searchResult");
+const viewpane = document.querySelector(".container > .albums");
+const player = document.querySelector("#player");
+
 var songList;
+var RECENT_LIST = [];
 
 searchField.addEventListener("input", (evt) => {
-    if (searchField.value.length > SEARCH_ACTIVATE_LEN) {
-        const searchKey = searchField.value;
-        searchSongs(searchKey);
+    console.log(evt);
+    if (evt.keyCode == 13) {
+        console.log("submit clicked");
     }
+});
+
+formField.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    //console.log(searchField.value);
+    searchSongs(searchField.value);
 });
 
 (function() {
@@ -22,40 +37,49 @@ searchField.addEventListener("input", (evt) => {
         .then(resp => resp.map(resp => resp.replace("E:/Songs/", "")))
         .then(resp => {songList = resp; return resp;})
         //.then(resp => resp.filter(resp => resp.includes("\\Arijit\\")))
-        .then(resp => resp.slice(0, 10))
+        .then(resp => resp.slice(0, PAGE_SIZE))
         
         .then(songs => {
             //console.log(songs);
             viewpane.innerHTML = songs.map(song => createCard(song)).join("");
         });
+    const recent = localStorage.getItem(RECENT_LIST_KEY);
+    if (recent) {
+        RECENT_LIST = RECENT_LIST.concat(recent);
+    }
+    console.log(RECENT_LIST);
 })();
 
-function play(songDiv, song) {
-    songDiv.innerHTML = `<audio controls autoplay="">
-        <source src="${CONTEXT + encodeURI(song)}" type="audio/mpeg" />
-    </audio>`;
+function play(song) {
+    player.setAttribute("src", CONTEXT + song);
+    player.setAttribute("autoplay", "");
+
+    if (RECENT_LIST.length > RECENT_QUEUE_LEN) {
+        RECENT_LIST.shift();
+    }
+    RECENT_LIST.push(song);
+    localStorage.setItem(RECENT_LIST_KEY, RECENT_LIST);
 }
 
 
 async function searchSongs(searchKey) {
+    //searchResultField.innerHTML = songList.filter(song => song.toUpperCase().includes(searchKey.toUpperCase())).slice(0, 3).join("<br>");
     viewpane.innerHTML = "";
-    viewpane.innerHTML = songList.filter(song => song.toUpperCase().includes(searchKey.toUpperCase())).slice(0, 10).map(song => createCard(song)).join("");
+    viewpane.innerHTML = songList.filter(song => song.toUpperCase().includes(searchKey.toUpperCase())).slice(0, PAGE_SIZE).map(song => createCard(song)).join("");
 }
 
 function createCard(song) {
-    const name = song.replace("/^.*[\\\/]/", '');
+    var name = song.substring(song.lastIndexOf("/")+1);
+    if (name.length > 35) {
+        name = name.substring(0, 35) + "...";
+    }
     return `
-        <div class="col-lg-4 col-sm-6 portfolio-item">
-            <div class="card h-100">
-            <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-            <div class="card-body">
-                <h4 class="card-title">
-                <a href="#">${name}</a>
-                </h4>
-                <p class="card-text"></p>
-                <div><button onclick="play(this.parentElement, \'${song}\')";><i class="fa fa-play"></i></button></div>
-            </div>
-            </div>
+    <div class="col-sm-12 m-1">
+    <div class="row" onclick="play('${song}')">
+        <div class="col-xs-9 mr-auto">
+          <a href="#">${name}</a>
         </div>
+    </div>
+    </div>
     `;
 }
